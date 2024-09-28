3f60e71c51f6ca7584db09ed814efe5f2714a099
dexter ramos <blitzdex27@gmail.com>
2024-09-29 00:26:58 +0800

    complete

--- Sources/DekiPersistable/DekiPersistable.swift
+++ Sources/DekiPersistable/DekiPersistable.swift
@@ -1,2 +1,38 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import Foundation
+
+public protocol DekiPersistable: Codable {
+    func save(fileName: String?) async throws -> Bool
+    static func load(fileName: String?) async throws -> Self
+}
+
+public extension DekiPersistable {
+    func save(fileName: String? = nil) async throws -> Bool {
+        let fileName = fileName ?? String(describing: type(of: self))
+        let url = URL.urlOnDocumentsDirectory(fileName: fileName)
+        let encoded = try JSONEncoder().encode(self)
+        try encoded.write(to: url)
+        return true
+    }
+    
+    static func load(fileName: String? = nil) async throws -> Self {
+        let fileName = fileName ?? String(describing: Self.self)
+        let url = URL.urlOnDocumentsDirectory(fileName: fileName)
+        let data = try Data(contentsOf: url)
+        let decoded = try JSONDecoder().decode(Self.self, from: data)
+        return decoded
+    }
+}
+
+extension URL {
+    static func urlOnDocumentsDirectory(fileName: String) -> URL {
+        if #available(iOS 16.0, *) {
+            return URL.documentsDirectory.appendingPathExtension(fileName)
+        } else {
+            let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)[0]
+            return documentsDirectory.appendingPathExtension(fileName)
+        }
+    }
+}
